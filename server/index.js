deddd8d6ecf71228cb30a5162654a05d56de3140
MinavKaria <minavpkaria@gmail.com>
2024-07-19 20:52:25 +0530

    chore: Add new pages and routes for different user roles

--- server/index.js
+++ server/index.js
@@ -1,16 +1,31 @@
 import express from "express"
+import router from "./routes/routes.js";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import cors from "cors";
 
 const app = express();
-const port = 3000;
+dotenv.config();
+const port=process.env.PORT || 3000;
 
-// Basic route
+const corsOptions = {
+  origin: 'http://localhost:5173',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,  
+  optionsSuccessStatus: 204,
+};
+
+app.use(bodyParser.json({limit:"30mb",extended:true}));
+app.use(bodyParser.urlencoded({limit:"30mb",extended:true}));  
+app.use(cors(corsOptions)); 
+app.use('/api', router);
+
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
 
-// Start the server
 app.listen(port, () => {
- console.log(`I am listening on +http://localhost:${port}`);
+ console.log(`Server listening on http://localhost:${port}`);
 
 });
 
