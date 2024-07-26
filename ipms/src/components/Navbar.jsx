09aaa683cff8cb8076379238b1090d6882ee01d0
MinavKaria <minavpkaria@gmail.com>
2024-07-26 19:51:04 +0530

    chore: Update Navbar component to include links for Login and Sign Up

--- ipms/src/components/Navbar.jsx
+++ ipms/src/components/Navbar.jsx
@@ -1,19 +1,20 @@
 import React from "react";
 import "../styles/Navbar.css";
+import {Link} from 'react-router-dom'
 
 function Navbar() {
   return (
     <>
       <div className="w-full top-nav">
         <div className="container mx-auto h-16 xl:px-5 flex items-center justify-between md:px-10">
-          <div className="text-3xl font-bold text-white flex font-saira gap-2 items-center">
+          <Link className="text-3xl font-bold text-white flex font-saira gap-2 items-center" to='/'>
             <div className="text-5xl">IPMS</div>
             <div>Digital Library</div>
-          </div>
+          </Link>
           <div className="text-white font-saira flex flex-row gap-10 items-center">
             <div className="text-xl">Help</div>
-            <div className="text-xl">Login</div>
-            <div className="text-2xl">Sign Up</div>
+            <Link className="text-xl" to="/sign">Login</Link>
+            <Link className="text-2xl" to="/sign">Sign Up</Link>
           </div>
         </div>
 
