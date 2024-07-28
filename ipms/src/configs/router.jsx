faa7eb082d6ba384c1b7c6335776d7cb7f8f08a2
MinavKaria <minavpkaria@gmail.com>
2024-07-28 13:19:32 +0530

    refactor: Update router configuration for author dashboard page

--- ipms/src/configs/router.jsx
+++ ipms/src/configs/router.jsx
@@ -3,11 +3,10 @@ import Landing from '../pages/Landing';
 import Layout from './../pages/Layout';
 import Contactus from '../pages/Contactus';
 import Login from '../pages/Login'
-import Dashboard from '../pages/Author/Dashboard';
 import Author1 from '../pages/Author/Author1';
 import Author2 from '../pages/Author/Author2';
 import Author3 from '../pages/Author/Author3';
-import Author4 from '../pages/Author/Author4';
+import AuthorDashboard from '../pages/Author/Author4';
 import Reviewer1 from '../pages/Reviewer/Reviewer1';
 import Orchid from '../pages/Orcid';
 
@@ -29,10 +28,6 @@ const router = createBrowserRouter([
         element: <Login/>
       },
       {
-        path:'/author/dashboard',
-        element:<Dashboard/>
-      },
-      {
         path:'/author/paperdetails/1',
         element:<Author1/>
       },
@@ -45,8 +40,8 @@ const router = createBrowserRouter([
         element:<Author3/>
       },
       {
-        path:'/author/paperdetails/4',
-        element:<Author4/>
+        path:'/author/dashboard',
+        element:<AuthorDashboard/>
       },
       {
         path:'/review/dashboard',
